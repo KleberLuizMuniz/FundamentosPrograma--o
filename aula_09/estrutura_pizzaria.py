1455e0b0b1dca90baeef0ed454e4004ac7031672
kleber <k4l3b3r@gmail.com>
2026-03-24 09:43:13 -0300

    Enviando atualização

---
+++ aula_09/estrutura_pizzaria.py
@@ -0,0 +1,13 @@
+# Variaveis da pizzaria 
+pizza_sabor = input("informe o sabor da pizza- [frango com requeijão], [calabresa], [mussarela], [banana com chocolate]: ")
+pizza_tamanho = input("informe o tamanho da pizza- [pequena], [media], [grande]: ")
+dia_semana = input("informe o dia da semana- [quarta], [quinta], [sexta], [sabado], [domingo]: ")
+
+print(f" O sabor escolhido da pizza é {pizza_sabor}, o tamanho é {pizza_tamanho} e hoje é {dia_semana}")
+
+# Promoções -> Estruturas condicionais 
+# comprando qualquer pizza e tamanho no sabado ganha refri gratuito
+
+# comprando uma pizza de calabresa de tamanho medio , em qualquer dia, frete gratis
+
+#comprando qualquer pizza de qualquer tamanho no domingo, o frete e o refri são gratuitos 
