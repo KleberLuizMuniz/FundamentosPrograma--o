e44c6158ab53efad5073a41866fcd9b9e71c8455
kleber <k4l3b3r@gmail.com>
2026-03-31 08:30:35 -0300

    atualização2

--- aula_10/estrutura_pizzaria_promocoes.py
+++ aula_10/estrutura_pizzaria_promocoes.py
@@ -7,8 +7,17 @@ dia_semana = input("informe o dia da semana- [quarta], [quinta], [sexta], [sabad
 print(f" O sabor escolhido da pizza é {pizza_sabor}, o tamanho é {pizza_tamanho} e hoje é {dia_semana}")
 
 # Promoções -> Estruturas condicionais 
+
 # comprando qualquer pizza e tamanho no sabado ganha refri gratuito
-
-# comprando uma pizza de calabresa de tamanho medio , em qualquer dia, frete gratis
-
+if dia_semana == "sabado":
+    print(f"🍕pedido aceito com sucesso")
+    print(f"o refri hoje é por conta da casa!")
 #comprando qualquer pizza de qualquer tamanho no domingo, o frete e o refri são gratuitos 
+elif dia_semana == "domingo":
+    print(f"🍕pedido aceito com sucesso")
+    print(f"o frete e o refri hoje é por conta da casa!")
+# comprando uma pizza de calabresa de tamanho medio , em qualquer dia, frete gratis
+elif pizza_sabor == "calabresa" and pizza_tamanho == "media":
+     print(f"🍕pedido aceito com sucesso")
+     print(f"o frete é por conta da casa!")
+
