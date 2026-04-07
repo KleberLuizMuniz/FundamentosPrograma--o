db17a759b313baebf4e5747ee1f0dd9660686286
Kleber <k4l3b3r@gmail.com>
2026-04-07 08:27:06 -0300

    subindo alterações

--- aula_10/estrutura_pizzaria_promocoes.py
+++ aula_10/estrutura_pizzaria_promocoes.py
@@ -1,15 +1,22 @@
+#Constante Fake
+SABORES_VALIDOS = ["frango com requeijão", "calabresa", "mussarela", "banana com chocolate"]
+TAMANHOS_VALIDOS = ["pequena", "media", "grande"]
+DIAS_VALIDOS = ["quarta", "quinta", "sexta", "sabado", "domingo"]
+
 # Variaveis da pizzaria 
 FRETE = 2 #Constante Fake 
-pizza_sabor = input("informe o sabor da pizza- [frango com requeijão], [calabresa], [mussarela], [banana com chocolate]: ")
-pizza_tamanho = input("informe o tamanho da pizza- [pequena], [media], [grande]: ")
-dia_semana = input("informe o dia da semana- [quarta], [quinta], [sexta], [sabado], [domingo]: ")
+
+pizza_sabor = input("informe o sabor da pizza- [frango com requeijão], [calabresa], [mussarela], [banana com chocolate]: ").lower().strip()
+pizza_tamanho = input("informe o tamanho da pizza- [pequena], [media], [grande]: ").lower().strip()
+dia_semana = input("informe o dia da semana- [quarta], [quinta], [sexta], [sabado], [domingo]: ").lower().strip()
 
 print(f" O sabor escolhido da pizza é {pizza_sabor}, o tamanho é {pizza_tamanho} e hoje é {dia_semana}")
 
 # Promoções -> Estruturas condicionais 
-
+if pizza_sabor not in SABORES_VALIDOS or pizza_tamanho not in TAMANHOS_VALIDOS or dia_semana not in DIAS_VALIDOS:
+    print(f"❌ Pedido não concluído: Uma ou mais opções informadas são inválidas.")
 # comprando qualquer pizza e tamanho no sabado ganha refri gratuito
-if dia_semana == "sabado":
+elif dia_semana == "sabado":
     print(f"🍕pedido aceito com sucesso")
     print(f"o refri hoje é por conta da casa!")
 #comprando qualquer pizza de qualquer tamanho no domingo, o frete e o refri são gratuitos 
@@ -20,4 +27,6 @@ elif dia_semana == "domingo":
 elif pizza_sabor == "calabresa" and pizza_tamanho == "media":
      print(f"🍕pedido aceito com sucesso")
      print(f"o frete é por conta da casa!")
+else:
+    print(f"pedido realizado com sucesso")
 
